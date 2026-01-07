dfdf4d6414dc54f808b4df8dd3050db2d9cfe3d7
sivaprasadtalapagala <sivaprasadreddytalapagala@gmail.com>
2026-01-07 20:52:40 +0530

    Revert "29_feat: setup http client and auth api integration"
    This reverts commit 47658665a7e5c1c1863ea74a21753bcdcdedfa1f.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
@@ -14,8 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
